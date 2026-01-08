13b927a7375dfb2ea928705cc220cdd16094821f
Lauwrens Reyneke <lauwrens@r-e-d.online>
2026-01-08 15:48:27 +0200

    Redirects cutoff API to team endpoint
    Removes the dedicated cutoff API endpoint and redirects requests to the team endpoint.
    
    This change consolidates functionality and simplifies the API structure by managing the standup cutoff time within the team endpoint.

--- api/manager/cutoff.ts
+++
@@ -1,6 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { json } from '../_lib/http.js'
-
-export default async function handler(_req: VercelRequest, res: VercelResponse) {
-  return json(res, 410, { error: 'Gone: use /api/manager/team (PUT { standupCutoffTime })' })
-}
